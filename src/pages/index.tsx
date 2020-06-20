da2d740a20c2f7fcc5eac128860b1b45363372de
Faisal Karim <faisalkarim96@gmail.com>
2020-06-20 12:13:38 +0700

    Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { NextPage } from "next";
-import { initializeApollo } from "@lib/apolloClient";
 import App from "@components/app";
 import Header from "@components/header";
 import MovieList from "@components/movie/movie-list";
 import Form from "@components/movie/form";
 import InfoBox from "@components/info-box";
-import { GET_MOVIES } from "@graphql/movie";
 
 const HomePage: NextPage = () => (
   <App>
@@ -15,20 +13,5 @@ const HomePage: NextPage = () => (
     <MovieList />
   </App>
 );
-
-export async function getStaticProps() {
-  const apolloClient = initializeApollo();
-
-  await apolloClient.query({
-    query: GET_MOVIES,
-  });
-
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract(),
-    },
-    unstable_revalidate: 1,
-  };
-}
 
 export default HomePage;
