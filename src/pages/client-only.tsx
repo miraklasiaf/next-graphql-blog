175e9cb720ecb4737437d852d8ae6a066c2f2525
Faisal Karim <faisalkarim96@gmail.com>
2020-06-20 12:01:49 +0700

    Fix

--- src/pages/client-only.tsx
+++ src/pages/client-only.tsx
@@ -4,18 +4,18 @@ import Header from "@components/header";
 import Form from "@components/movie/form";
 import MovieList from "@components/movie/movie-list";
 
-const ClientOnlyPage = () => (
-  <App>
-    <Header />
-    <InfoBox>
-      ℹ️ This page shows how use Apollo only in the client. If you{" "}
-      <a href="/client-only">reload</a> this page, you will see a loader since
-      Apollo didn't fetch any data on the server. This is useful when the page
-      doesn't have SEO requirements or blocking data fetching requirements.
-    </InfoBox>
-    <Form />
-    <MovieList />
-  </App>
-);
-
-export default ClientOnlyPage;
+export default function ClientOnlyPage() {
+  return (
+    <App>
+      <Header />
+      <InfoBox>
+        ℹ️ This page shows how use Apollo only in the client. If you{" "}
+        <a href="/client-only">reload</a> this page, you will see a loader since
+        Apollo didn't fetch any data on the server. This is useful when the page
+        doesn't have SEO requirements or blocking data fetching requirements.
+      </InfoBox>
+      <Form />
+      <MovieList />
+    </App>
+  );
+}
